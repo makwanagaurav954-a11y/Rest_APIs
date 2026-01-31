4a0a6a3c56f2e9873754f6069ad05d39531259b3
Gaurav Makwana <makwanagaurav954@gmail.com>
2026-01-31 15:19:49 +0530

    Add dynamic PORT for deployment

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require("path");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
